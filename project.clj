03eaf8b538300183903aad4100323105b516bec0
Niko Sten <niko.sten@gmail.com>
2014-09-27 09:27:10 +0300

    Compojure template

--- project.clj
+++ project.clj
@@ -1,6 +1,13 @@
 (defproject yak "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://yak.clojurecup.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]]
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler yak.handler/app
+         :port 80}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
