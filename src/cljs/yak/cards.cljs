2c6f7d05f9fbe0fc03831e38aa90a5cace874925
Niko Sten <niko.sten@gmail.com>
2014-09-28 12:44:38 +0300

    Cards progress

--- src/cljs/yak/cards.cljs
+++ src/cljs/yak/cards.cljs
@@ -2,11 +2,30 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [reagent-bootstrap :as b]))
 
-(defn cards-view [cards]
-  [b/list-group
-    (for [id card]
-      [b/clickable-list-group-item
-        [:span {:on-click #(.log js/console "card clicked")} "A card"]
-        "Short text ..."])
-    (b/clickable-list-group-item
-      [:span {:on-click #(.log js/console "New card")} "Add new card"])])
+(defn create-card [cards card]
+  (swap! cards conj card))
+
+(defn edit-card-form []
+  )
+
+(defn show-create-card-dialog [cards]
+  (let [new-card (atom {:title ""
+                         :description ""})]
+    (b/open-modal
+     [b/modal
+      "Create card" ; Title
+      [edit-card-form new-card] ; Body
+      [:div [b/modal-close-button "Close"] ; Footer
+       [b/primary-button
+        {:on-click #(create-card! cards @new-card)
+         :data-dismiss "modal"} "Create"]]])))
+
+(defn cards-view [cards-input]
+  (let [cards (atom cards-input)]
+    [b/list-group
+      (for [card @cards]
+        [b/clickable-list-group-item
+          [:span {:on-click #(.log js/console "card clicked")} "A card"]
+          "Short text ..."])
+      (b/clickable-list-group-item
+        [:span {:on-click #(show-create-card-dialog cards)} "Add new card"])]))
